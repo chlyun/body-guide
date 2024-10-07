660ff46cadbb211dd3e968498500c223fc8d8c6c
chlyun <rkdmf1306@gmail.com>
2024-10-07 21:24:21 +0900

    해결버전

--- src/types/nutrient_result.tsx
+++ src/types/nutrient_result.tsx
@@ -1,20 +1,21 @@
-interface NutrientResult{
-    BMI : string;
-    BMR : number;
-    TDEE : number;
-    composition : NutrientComposition;
-    mealTimes : string[];
+interface NutrientResult {
+  BMI: string;
+  DIETGOL: string;
+  BMR: number;
+  TDEE: number;
+  composition: NutrientComposition;
+  mealTimes: string[];
 }
 
-interface NutrientComposition{
-    carbohydrate : NutrientProfile;      // 탄수화물
-    protein : NutrientProfile;           // 단백질
-    unFat : NutrientProfile;             // 불포화지방
-    satFat : NutrientProfile;            // 포화지방
+interface NutrientComposition {
+  carbohydrate: NutrientProfile; // 탄수화물
+  protein: NutrientProfile; // 단백질
+  unFat: NutrientProfile; // 불포화지방
+  satFat: NutrientProfile; // 포화지방
 }
 
-interface NutrientProfile{
-    ratio : number;                      // 식단에서 비율
-    calory : number;                     // 칼로리
-    gram : number;                       // 그램
+interface NutrientProfile {
+  ratio: number; // 식단에서 비율
+  calory: number; // 칼로리
+  gram: number; // 그램
 }
